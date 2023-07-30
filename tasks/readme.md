bf70d589cb144779c02b29e9fd818f88f56ed94c
JayThibs <thibo.jacques@gmail.com>
2023-07-30 01:27:09 -0400

    Setting up repo.

---
+++ tasks/readme.md
@@ -0,0 +1,3 @@
+# Linting and Testing
+
+This is a directory meant for linting and testing our code.
---
+++ unsupervised_behavioural_clustering/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import json
+import time
+import random
+import argparse
+import pickle
+import tqdm
+import matplotlib.pyplot as plt
+import scipy
+import sklearn
+import glob
+import pandas as pd
+import numpy as np
+import openai
+import langchain
+from matplotlib.patches import Rectangle
+import matplotlib.patches as mpatches
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans, OPTICS
+from terminaltables import AsciiTable, SingleTable
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from scipy.cluster.hierarchy import dendrogram, linkage, to_tree
+
+# Set up OpenAI API key
+openai.api_key = os.environ["OPENAI_API_KEY"]
