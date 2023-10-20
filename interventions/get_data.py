8c2a3ed445bfd3a7b74a3bc2189ffd8c48b5d0be
Quintin Pope <popeq@oregonstate.edu>
2023-10-20 14:41:24 -0700

    Adding code for GPT2 finetuning interventions

---
+++ interventions/get_data.py
@@ -0,0 +1,212 @@
+import torch
+import transformers
+from transformers import AutoTokenizer
+from datasets import load_dataset
+import pandas as pd
+from convo_tree_helpers import extract_convo_trees
+
+
+
+def get_mnli_dataset(model_name = "bert-base-uncased", 
+                     longest_sequence_allowed = 64, 
+                     n_test_data = 10000, 
+                     make_binary = True, 
+                     data_path = "data/mnli", 
+                     device = "cuda:0", 
+                     save_data = True,
+                     data_label = "mnli",
+                     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")):
+    mnli_dataset = load_dataset("multi_nli")
+    mnli_pandas = mnli_dataset['train'].to_pandas()[["premise", "hypothesis", "label"]]
+    mnli_pandas = pd.concat((mnli_pandas, mnli_dataset['validation_matched'].to_pandas()[["premise", "hypothesis", "label"]]), ignore_index=True)
+    mnli_pandas = pd.concat((mnli_pandas, mnli_dataset['validation_mismatched'].to_pandas()[["premise", "hypothesis", "label"]]), ignore_index=True)
+    mnli_pandas = mnli_pandas.sample(frac = 1, random_state=42)
+
+    train_data, train_labels, test_data, test_labels = filter_data(mnli_pandas, 
+                                                                   tokenizer,
+                                                                   longest_sequence_allowed,
+                                                                   make_binary,
+                                                                   n_test_data,
+                                                                   device)
+    if save_data:
+        save_data_to_file(train_data, 
+                          train_labels, 
+                          test_data, 
+                          test_labels, 
+                          data_path,
+                          data_label)
+    return train_data, train_labels, test_data, test_labels
+
+def get_anli_dataset(model_name = "bert-base-uncased", 
+                     longest_sequence_allowed = 64, 
+                     n_test_data = 10000, 
+                     make_binary = True, 
+                     data_path = "data/anli", 
+                     device = "cuda:0", 
+                     save_data = True,
+                     data_label = "anli",
+                     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")):
+    anli_dataset = load_dataset("anli")
+    anli_pandas = anli_dataset['train_r3'].to_pandas()[["premise", "hypothesis", "label"]]
+    anli_pandas = pd.concat((anli_pandas, anli_dataset['test_r3'].to_pandas()[["premise", "hypothesis", "label"]]), ignore_index=True)
+    anli_pandas = pd.concat((anli_pandas, anli_dataset['dev_r3'].to_pandas()[["premise", "hypothesis", "label"]]), ignore_index=True)
+    anli_pandas = anli_pandas.sample(frac = 1, random_state=42)
+    train_data, train_labels, test_data, test_labels = filter_data(anli_pandas, 
+                                                                   tokenizer,
+                                                                   longest_sequence_allowed,
+                                                                   make_binary,
+                                                                   n_test_data,
+                                                                   device)
+    print(len(train_data), len(test_data))
+    if save_data:
+        save_data_to_file(train_data, 
+                          train_labels, 
+                          test_data, 
+                          test_labels, 
+                          data_path,
+                          data_label)
+    return train_data, train_labels, test_data, test_labels
+
+def get_open_assistant_dataset(model_name = "gpt2",
+                     longest_sequence_allowed = 512,
+                     n_test_data = 10000,
+                     data_path = "data/open_assistant",
+                     device = "cuda:0",
+                     save_data = True,
+                     data_label = "open_assistant",
+                     tokenizer = AutoTokenizer.from_pretrained("gpt2")):
+
+    open_assistant_dataset = load_dataset("OpenAssistant/oasst1")
+    df_train = open_assistant_dataset['train'].to_pandas()
+    df_test = open_assistant_dataset['validation'].to_pandas()
+
+    train_convos = extract_convo_trees(df_train)
+    test_convos = extract_convo_trees(df_test)
+
+    train_convos_ids = torch.tensor(tokenizer.batch_encode_plus(train_convos, padding = "longest", max_length = longest_sequence_allowed, truncation = True)['input_ids']).to(device)
+    test_convos_ids = torch.tensor(tokenizer.batch_encode_plus(test_convos, padding = "longest", max_length = longest_sequence_allowed, truncation = True)['input_ids']).to(device)
+
+
+    if save_data:
+        train_labels, test_labels = None, None
+        save_data_to_file(train_convos_ids,
+                          train_labels,
+                          test_convos_ids,
+                          test_labels,
+                          data_path,
+                          data_label)
+
+    return train_convos_ids, train_convos_ids, test_convos_ids, test_convos_ids
+
+
+def load_data(model_name, dataset_label, load_saved_data, data_path, longest_sequence_allowed, test_set_size, make_binary, device):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    if dataset_label == "mnli":
+        if str(load_saved_data) == "true":
+            train_data, train_labels, test_data, test_labels = load_data_from_file(data_path, "mnli")
+        else:
+            train_data, train_labels, test_data, test_labels = get_mnli_dataset(model_name = model_name,
+                                                                                longest_sequence_allowed = longest_sequence_allowed,
+                                                                                n_test_data = test_set_size,
+                                                                                make_binary = make_binary,
+                                                                                data_path = data_path,
+                                                                                device = device,
+                                                                                save_data = True,
+                                                                                tokenizer = tokenizer)
+    elif dataset_label == "anli":
+        if str(load_saved_data) == "true":
+            train_data, train_labels, test_data, test_labels = load_data_from_file(data_path, "anli")
+        else:
+            train_data, train_labels, test_data, test_labels = get_anli_dataset(model_name = model_name,
+                                                                                longest_sequence_allowed = longest_sequence_allowed,
+                                                                                n_test_data = test_set_size,
+                                                                                make_binary = make_binary,
+                                                                                data_path = data_path,
+                                                                                device = device,
+                                                                                save_data = True,
+                                                                                tokenizer = tokenizer)
+
+    elif dataset_label == "open_assistant":
+        if str(load_saved_data) == "true":
+            train_data, train_labels, test_data, test_labels = load_data_from_file(data_path, "open_assistant")
+        else:
+            train_data, train_labels, test_data, test_labels = get_open_assistant_dataset(model_name = model_name,
+                                                                                longest_sequence_allowed = longest_sequence_allowed,
+                                                                                n_test_data = test_set_size,
+                                                                                data_path = data_path,
+                                                                                device = device,
+                                                                                save_data = True,
+                                                                                tokenizer = tokenizer)
+    return train_data, train_labels, test_data, test_labels, tokenizer
+
+
+
+def save_data_to_file(train_data, 
+                      train_labels, 
+                      test_data, 
+                      test_labels, 
+                      path,
+                      dataset_label):
+    data_save_relative_path = path + "/" + dataset_label
+    print("Attempting to save data to relative path: \"" + data_save_relative_path + "_[stuff].pth" + "\"")
+    torch.save(train_data, data_save_relative_path + "_training_data.pth")
+    if not train_labels is None:
+        torch.save(train_labels, data_save_relative_path + "_training_labels.pth")
+    torch.save(test_data, data_save_relative_path + "_testing_data.pth")
+    if not test_labels is None:
+        torch.save(test_labels, data_save_relative_path + "_testing_labels.pth")
+
+def load_data_from_file(path, dataset_label):
+    data_load_relative_path = path + "/" + dataset_label
+    print("Attempting to load data from relative path: \"" + data_load_relative_path + "_[stuff].pth" + "\"")
+    train_data = torch.load(data_load_relative_path + "_training_data.pth")
+    test_data = torch.load(data_load_relative_path + "_testing_data.pth")
+    if not str.lower(dataset_label) in ['open_assistant']:
+        train_labels = torch.load(data_load_relative_path + "_training_labels.pth")
+        test_labels = torch.load(data_load_relative_path + "_testing_labels.pth")
+    else:
+        train_labels, test_labels = train_data, test_data
+    return train_data, train_labels, test_data, test_labels
+
+def tokenize_data(data, tokenizer, max_length = 128):
+    text_data = []
+    for i in range(len(data)):
+        current_text = data.iloc[i]["premise"] + " [SEP] " + data.iloc[i]["hypothesis"]
+        text_data.append(current_text)
+    return torch.tensor(tokenizer.batch_encode_plus(text_data, padding = "longest", max_length = max_length, truncation = True)['input_ids'])
+
+def filter_data(data, 
+                tokenizer,
+                longest_sequence_allowed = 64,
+                make_binary = True,
+                n_test_data = 10000,
+                device = "cpu"):
+    current_tokens = tokenize_data(data, tokenizer, max_length = longest_sequence_allowed + 10)
+    inputs_too_long_flag = torch.sum(current_tokens > 0, dim = 1) <= longest_sequence_allowed
+    inputs_short_enough_entries = [i for i in range(len(data)) if inputs_too_long_flag[i]]
+
+    short_mnli_data = data.iloc[inputs_short_enough_entries]
+    short_mnli_labels = torch.from_numpy(data.iloc[inputs_short_enough_entries]["label"].values)
+    if make_binary:
+        binary_labels_selector_flag = short_mnli_labels != 1
+        inputs_only_entailment_or_contradiction_entries = [i for i in range(len(short_mnli_labels)) if binary_labels_selector_flag[i]]
+        selected_labels = (short_mnli_labels[inputs_only_entailment_or_contradiction_entries] / 2).to(int)
+        selected_tokens = tokenize_data(short_mnli_data.iloc[inputs_only_entailment_or_contradiction_entries], tokenizer)
+    else:
+        selected_labels = short_mnli_labels
+        selected_tokens = tokenize_data(short_mnli_data, tokenizer)
+
+    train_set_size = len(selected_tokens) - n_test_data
+    random_indices_perm = torch.randperm(len(selected_tokens))
+    train_set_selection_indices = random_indices_perm[:train_set_size]
+    test_set_selection_indices = random_indices_perm[train_set_size:]
+
+    train_data = selected_tokens[train_set_selection_indices].to(device)
+    train_labels = selected_labels[train_set_selection_indices].to(device)
+    test_data = selected_tokens[test_set_selection_indices].to(device)
+    test_labels = selected_labels[test_set_selection_indices].to(device)
+    return train_data, train_labels, test_data, test_labels
---
+++ interventions/train.py
@@ -0,0 +1,133 @@
+import torch
+import transformers
+from transformers import AutoModel, AutoModelForSequenceClassification, AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+#import pandas as pd
+import argparse
+import os
+
+from connectivity_helpers import *
+from train_helpers import *
+from get_data import *
+
+parser = argparse.ArgumentParser(description='Model training script for linear basin connectivity experiments.')
+
+
+parser.add_argument('--model_name', type=str, default='bert-base-uncased',
+                    help='Huggingface pretrained language model name.')
+
+parser.add_argument('--dataset', type=str, default='mnli',
+                    help='Huggingface dataset name to train the model on. Can also be a path to a saved dataset.')
+
+parser.add_argument('--load_saved_data', type=str, default='true',
+                    help='Do we try to load a saved dataset, or re-download the data?')
+
+parser.add_argument('--longest_sequence_allowed', type=int, default=64,
+                    help='Ignore datapoints with more than this many tokens.')
+
+parser.add_argument('--test_set_size', type=int, default=10000,
+                    help='Number of datapoints to include in the test set.')
+
+parser.add_argument('--make_binary', type=str, default='true',
+                    help='Do we convert the datasets to a binary prediction problem? Any value but "true" means we do not.')
+
+parser.add_argument('--existing_models_path', type=str, default='models_to_avoid',
+                    help='Directory with existing models to regularize away from.')
+
+parser.add_argument('--optimizer', type=str, default='adamw',
+                    help='Optimizer for training.')
+
+parser.add_argument('--lr', type=float, default=2e-5,
+                    help='Learning rate for training.')
+
+parser.add_argument('--weight_decay', type=float, default=0.01,
+                    help='Weight decay during training.')
+
+parser.add_argument('--num_schedule_cycles', type=int, default=1,
+                    help='Number of cycles for learning rate scheduler.')
+
+parser.add_argument('--batch_size', type=int, default=32,
+                    help='Batch size during training.')
+
+parser.add_argument('--epochs', type=int, default=3,
+                    help='Number of training epochs.')
+
+parser.add_argument('--warmup_frac', type=float, default=0.1,
+                    help='Fraction of training to spend on learning rate warmup.')
+
+parser.add_argument('--max_interpolation_model_loss', type=float, default=1.0,
+                    help='Maximum value of the basin exploration regularization loss (before weighting).')
+
+parser.add_argument('--basin_exploration_loss_weight', type=float, default=1.0,
+                    help='Weight of the basin exploration regularization loss.')
+
+parser.add_argument('--device', type=str, default="cuda:0",
+                    help='GPU / CPU device on which experiments will run.')
+
+parser.add_argument('--model_save_name', type=str, default="None",
+                    help='Name given to saved models. If set to \"None\", model will not be saved.')
+
+parser.add_argument('--model_save_loc', type=str, default="intervention_models",
+                    help='Folder in which models are saved.')
+
+parser.add_argument('--models_to_save', type=str, default="last",
+                    help='Which of the trained models to save. Options are \'last\', \'best\', or \'both\'. Defaults to \'last\'.')
+
+args = parser.parse_args()
+
+dataset_label = str.lower(args.dataset)
+
+training_generator = False
+if dataset_label in ['open_assistant']:
+    training_generator = True
+    model = AutoModelForCausalLM.from_pretrained(args.model_name).to(args.device)
+else:
+    model = AutoModelForSequenceClassification.from_pretrained(args.model_name).to(args.device)
+
+data_path = "data/" + dataset_label
+make_binary = str.lower(args.make_binary) == "true"
+
+
+train_data, train_labels, test_data, test_labels, tokenizer = load_data(args.model_name,
+                                                                        dataset_label,
+                                                                        args.load_saved_data,
+                                                                        data_path,
+                                                                        args.longest_sequence_allowed,
+                                                                        args.test_set_size,
+                                                                        make_binary,
+                                                                        args.device)
+
+
+old_model_names = [name for name in os.listdir(args.existing_models_path) if name[0] != '.']
+existing_models = [torch.load(args.existing_models_path + "/" + old_model_name) for old_model_name in old_model_names]
+
+print("Currently regularizing against", len(existing_models), "existing models.")
+if len(existing_models) > 0:
+    print("Existing models:", str(old_model_names))
+model, best_model, best_model_test_loss, train_losses, train_accuracies, test_losses, test_accuracies = train_new_model(model,
+                                                                                     train_data,
+                                                                                     train_labels,
+                                                                                     test_data,
+                                                                                     test_labels,
+                                                                                     training_generator = training_generator,
+                                                                                     existing_models = existing_models,
+                                                                                     optimizer = args.optimizer,
+                                                                                     lr = args.lr,
+                                                                                     weight_decay = args.weight_decay,
+                                                                                     num_schedule_cycles = args.num_schedule_cycles,
+                                                                                     batch_size = args.batch_size,
+                                                                                     epochs = args.epochs,
+                                                                                     warmup_frac = args.warmup_frac,
+                                                                                     max_interpolation_model_loss = args.max_interpolation_model_loss,
+                                                                                     basin_exploration_loss_weight = args.basin_exploration_loss_weight,
+                                                                                     device = args.device,
+                                                                                     pad_token_id = tokenizer.pad_token_id)
+models_to_save = str.lower(args.models_to_save)
+
+if args.model_save_name != "None":
+    if models_to_save in ['last', 'both']:
+        torch.save(model, args.model_save_loc + "/" + args.model_save_name + "_last_model.pth")
+    if models_to_save in ['best', 'both']:
+        torch.save(best_model, args.model_save_loc + "/" + args.model_save_name + "_best_model.pth")
+
+print(test_accuracies)
