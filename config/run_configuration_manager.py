2246ad26ad2b79991f25e709a64abd309d30aa66
Jacques Thibodeau <thibo.jacques@gmail.com>
2024-04-05 23:56:43 -0300

    Moved the template test run for quick modification.

--- config/run_configuration_manager.py
+++ config/run_configuration_manager.py
@@ -32,6 +32,33 @@ class RunConfigurationManager:
                 test_mode=True,
             ),
             ############################################
+            # Testing configurations
+            # Use this to play around with different settings before saving them as a new configuration
+            ############################################
+            "test": RunSettings(
+                name="test",
+                model_settings=ModelSettings(
+                    models=[
+                        ("openai", "gpt-3.5-turbo"),
+                        ("openai", "gpt-4"),
+                        ("anthropic", "claude-v1"),
+                    ]
+                ),
+                data_settings=DataSettings(n_statements=300, reuse_data=["all"]),
+                prompt_settings=PromptSettings(
+                    approval_prompts=[
+                        "Briefly list the common themes of the following texts:",
+                    ]
+                ),
+                plot_settings=PlotSettings(
+                    hide_plots=["tsne", "approvals", "hierarchical", "awareness"]
+                ),
+                clustering_settings=ClusteringSettings(
+                    n_clusters=3,
+                ),
+                tsne_settings=TsneSettings(perplexity=30),
+            ),
+            ############################################
             # Full run configurations
             ############################################
             "full_run": RunSettings(
@@ -54,29 +81,6 @@ class RunConfigurationManager:
                 data_settings=DataSettings(n_statements=100, reuse_data=["all"]),
                 test_mode=True,
                 skip_sections=["approvals"],
-            ),
-            "template": RunSettings(
-                name="template",
-                model_settings=ModelSettings(
-                    models=[
-                        ("openai", "gpt-3.5-turbo"),
-                        ("openai", "gpt-4"),
-                        ("anthropic", "claude-v1"),
-                    ]
-                ),
-                data_settings=DataSettings(n_statements=300, reuse_data=["all"]),
-                prompt_settings=PromptSettings(
-                    approval_prompts=[
-                        "Briefly list the common themes of the following texts:",
-                    ]
-                ),
-                plot_settings=PlotSettings(
-                    hide_plots=["tsne", "approvals", "hierarchical", "awareness"]
-                ),
-                clustering_settings=ClusteringSettings(
-                    n_clusters=3,
-                ),
-                tsne_settings=TsneSettings(perplexity=30),
             ),
         }
 
