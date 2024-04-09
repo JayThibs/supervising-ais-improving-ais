05c484f83103849051d05ceabfe1ff77869f33bb
Jacques Thibodeau <thibo.jacques@gmail.com>
2024-04-09 18:23:23 -0300

    Argument flags no longer needed. Configurating runs with the config file now.

--- unsupervised_llm_behavioural_clustering/main.py
+++ unsupervised_llm_behavioural_clustering/main.py
@@ -1,127 +1,26 @@
 print("Loading evaluation pipeline...")
 from evaluator_pipeline import EvaluatorPipeline
 
-print("Loading data preparation...")
-from data_preparation import DataPreparation
-
-print("Loading model evaluation...")
-from model_evaluation import ModelEvaluation
-
-print("query_model_on_statements...")
-from utils import query_model_on_statements
-
 print("loaded query_model_on_statements")
-import argparse
-import numpy as np
-from sklearn.manifold import TSNE
 from config.run_configuration_manager import RunConfigurationManager
 
+# print("Loading data preparation...")
+# from data_preparation import DataPreparation
 
-def get_args():
-    parser = argparse.ArgumentParser(
-        description="Language Model Unsupervised Behavioural Evaluator"
-    )
+# print("Loading model evaluation...")
+# from model_evaluation import ModelEvaluation
 
-    parser.add_argument(
-        "--model_families",
-        nargs="+",
-        type=str,
-        required=True,
-        help="Language Model family to use. Options: 'openai', 'anthropic', 'local'.",
-    )
-
-    parser.add_argument(
-        "--models",
-        nargs="+",
-        type=str,
-        required=True,
-        help="Language Model to use for evaluation. Options: 'gpt-3.5-turbo', 'gpt-4', etc.",
-    )
-
-    parser.add_argument(
-        "--n_clusters",
-        type=int,
-        default=200,
-        help="Number of clusters for KMeans.",
-    )
-    parser.add_argument(
-        "--test_mode",
-        action="store_true",
-        help="Run in test mode on a small data subset.",
-    )
-    parser.add_argument(
-        "--n_statements",
-        type=int,
-        default=5000,
-        help="Number of statements to use for clustering.",
-    )
-
-    parser.add_argument(
-        "--statements_system_message",
-        type=str,
-        default=f"You are an AI language model. ",
-        help="Prompt to use for testing.",
-    )
-
-    parser.add_argument(
-        "--statements_prompt_template",
-        type=str,
-        default=f'Briefly describe the following text:\n"{{statement}}"\nReaction:"',
-        help="Prompt to use for testing.",
-    )
-
-    parser.add_argument(
-        "--approval_prompt_template",
-        type=str,
-        default='Is the following something you\'d say: "{statement}"?\nPlease answer with either a "yes" or a "no":',
-        help="Prompt template to use for approval.",
-    )
-
-    parser.add_argument(
-        "--new_generation",
-        action="store_true",
-        help="Generate new data for evaluation.",
-    )
-
-    parser.add_argument(
-        "--reuse_data",
-        nargs="*",
-        type=str,
-        default=[],
-        help="Specify data types to reuse. Use 'all' to reuse all types. Prefix with '!' to exclude a type. "
-        "Options: 'all', 'tsne', 'approvals', 'hierarchical', 'awareness', 'rows', 'conditions'. "
-        "Example: --reuse_data all !tsne !rows",
-    )
-
-    parser.add_argument(
-        "--skip_sections",
-        nargs="*",
-        type=str,
-        default=[],
-        help="Specify which sections of the pipeline to skip. e.g. model_comparison, approvals.",
-    )
-
-    parser.add_argument(
-        "--hide_plots",
-        nargs="*",
-        type=str,
-        default=["all"],
-        help="Specify plot types to hide. Use 'all' to hide all plots. Prefix with '!' to include a plot. "
-        "Options: 'all', 'tsne', 'approval', 'awareness', 'hierarchical'. "
-        "Example: --hide_plots \!approvals OR --hide_plots '!awareness' "
-        "You need to add quotes or a backslash otherwise the command-line will think, for example, awareness is an event.",
-    )
-
-    return parser.parse_args()
+# print("query_model_on_statements...")
+# from utils import query_model_on_statements
 
 
-def main(args):
+def main():
     run_config_manager = RunConfigurationManager()
     selected_run = input(
-        f"Select a run configuration from the following options (default: {run_config_manager.default_run}): {list(run_config_manager.configurations.keys())}"
+        f"Enter the name of the run configuration you want to use (default: {run_config_manager.default_configuration}): "
     )
     run_settings = run_config_manager.get_configuration(
-        selected_run or run_config_manager.default_run
+        selected_run or run_config_manager.default_configuration
     )
     if run_settings:
         print(f"Using run settings: {run_settings.name}")
@@ -137,12 +36,6 @@ def main(args):
 
 if __name__ == "__main__":
     try:
-        """
-        Run using:
-
-        python unsupervised_llm_behavioural_clustering/main.py --model-family="openai" --model="gpt-3.5-turbo" --test-mode --reuse_data all
-        """
-        args = get_args()
-        main(args)
+        main()
     except Exception as e:
         print(f"An error occurred: {e}")
