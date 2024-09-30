de7efd9a8eb29b8740d87edef005e425dce77fee
Quintin Pope <popeq@oregonstate.edu>
2024-09-30 12:05:50 -0700

    changed args

--- src/interventions/auto_finetune_eval/run_auto_finetuning_main.sh
+++ src/interventions/auto_finetune_eval/run_auto_finetuning_main.sh
@@ -1,10 +1,11 @@
 CUDA_VISIBLE_DEVICES=0 python auto_finetuning_main.py \
     --base_model "NousResearch/Meta-Llama-3-8B" \
-    --num_samples 10 \
-    --num_ground_truths 2 \
+    --num_samples 100 \
+    --num_ground_truths 5 \
     --api_provider "anthropic" \
     --model_str "claude-3-haiku-20240307" \
     --key_path "../../../data/api_keys/anthropic_key.txt" \
     --output_file_path "../../../data/training_data/debug_autofinetune_data.csv" \
-    --focus_area "natural language processing" \
-    --finetuning_params '{"learning_rate": 5e-5, "num_epochs": 3}'
+    --focus_area "weird historical facts" \
+    --finetuning_params '{"learning_rate": 5e-5, "num_epochs": 2}' \
+    --device "cuda:0"
